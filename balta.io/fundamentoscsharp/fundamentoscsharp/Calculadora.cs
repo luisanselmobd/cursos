08697cce27d557ffe7cf863af5ee37c83e298462
luisanselmobd <luisanselmo.dev@gmail.com>
2024-01-15 17:43:20 -0300

    Início das atividades do curso balta.io

---
+++ balta.io/fundamentoscsharp/fundamentoscsharp/Calculadora.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fundamentoscsharp
+{
+    class Calculadora
+    {
+        private int resultado;
+
+        public int Resultado { get => resultado; set => resultado = value; }
+
+        public int RealizarOperacao(int a, int b, operacoes operacao)
+        {   
+            switch (operacao)
+            {
+                case operacoes.somar:
+                    Resultado = somar(a, b);
+                    break;
+                case operacoes.subtrair:
+                    Resultado = subtrair(a, b);
+                    break;
+                case operacoes.dividir:
+                    Resultado = dividir(a, b);
+                    break;
+                case operacoes.multiplicar:
+                    Resultado = multiplicar(a, b);
+                    break;
+            }
+            return Resultado; 
+        }
+
+        int somar(int a, int b) { return a + b; }
+        int subtrair(int a, int b) { return a - b; }
+        int dividir(int a, int b) { return a / b;}
+        int multiplicar(int a, int b) { return a * b; }
+
+        
+    }
+
+    enum operacoes
+    {
+        somar,
+        subtrair,
+        dividir,
+        multiplicar
+    }
+
+}
---
+++ balta.io/fundamentoscsharp/fundamentoscsharp/Program.cs
@@ -0,0 +1,4 @@
+﻿using fundamentoscsharp;
+
+Calculadora c = new Calculadora();
+Console.WriteLine(c.RealizarOperacao(1, 2, operacoes.multiplicar));
